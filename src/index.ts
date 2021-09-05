fef2f1b2f1b75897356e0bc1e0fbde03e69fb0cb
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-09-05 19:09:46 -0300

    set .env dev file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import './config';
 import express, { Application } from 'express';
+
 const app: Application = express();
 
-const PORT: string | number = process.env.PORT || 5000;
-
+const PORT: string | undefined = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server start on port ${PORT}`)
 });
